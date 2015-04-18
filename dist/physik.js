0df42cb1ed3cf8741ab31671b6fed0e13548ec96
Luca Keidel <info@lucakeidel.de>
2015-04-18 15:06:23 +0200

    Updated compiled files

--- dist/physik.js
+++ dist/physik.js
@@ -1,5 +1,5 @@
 (function() {
-  var ErrorInterval, aufg10, aufg11, aufg11_example, aufg12, aufg4, aufg6, aufg8, aufg9, cos, createFromAnalogMeasurement, createFromDigitalMeasurement, decimalPlaces, log10, significantDigitsCeiling, sin, tan;
+  var ErrorInterval, cos, createFromAnalogMeasurement, createFromDigitalMeasurement, decimalPlaces, log10, significantDigitsCeiling, sin, tan;
 
   decimalPlaces = function(num) {
     var match;
@@ -129,62 +129,6 @@
     return new ErrorInterval(val.median, da).intermediateResult();
   };
 
-  aufg4 = function() {
-    return new ErrorInterval(52.684063, 0.0176228).endResult();
-  };
-
-  aufg6 = function() {
-    return new ErrorInterval(34.7, 7.6).relativeError();
-  };
-
-  aufg8 = function() {
-    var l1, l2, l3;
-    l1 = new ErrorInterval(200, 0.5);
-    l2 = new ErrorInterval(200, 0.5);
-    l3 = new ErrorInterval(104.7, 1.5);
-    return l1.add(l2).add(l3).endResult();
-  };
-
-  aufg9 = function() {
-    var s, t;
-    s = new ErrorInterval(100, 10);
-    t = new ErrorInterval(11.2, 0.3);
-    return s.div(t).scalar(3.6).endResult();
-  };
-
-  aufg10 = function() {
-    var r, t0, t1;
-    r = new ErrorInterval(1.000, 0.002);
-    t0 = new ErrorInterval(20.00, 0.11);
-    t1 = new ErrorInterval(2.4, 0.2);
-    return t0.div(t1).pow(0.5).mult(r).endResult();
-  };
-
-  aufg11_example = function() {
-    var a, b, c;
-    a = new ErrorInterval(62.4, 0.2);
-    b = new ErrorInterval(11.2, 0.2);
-    c = new ErrorInterval(9.2, 0.2);
-    return a.sub(b).div(c).endResult();
-  };
-
-  aufg11 = function() {
-    var s, t;
-    t = new ErrorInterval(71, 2);
-    s = new ErrorInterval(400, 5);
-    return new ErrorInterval(1000, 0).div(s.div(t)).endResult();
-  };
-
-  aufg12 = function() {
-    var alpha, mu, sinAlpha;
-    mu = new ErrorInterval(632.8, 0);
-    alpha = new ErrorInterval(13.4, 0.5);
-    sinAlpha = alpha.apply(function(val) {
-      return Math.sin(val * (Math.PI / 180));
-    });
-    return mu.div(sinAlpha).scalar(0.001).endResult();
-  };
-
   sin = function(v) {
     return Math.sin(v * (Math.PI / 180));
   };
@@ -199,14 +143,6 @@
 
   module.exports = {
     ErrorInterval: ErrorInterval,
-    aufg4: aufg4,
-    aufg6: aufg6,
-    aufg8: aufg8,
-    aufg9: aufg9,
-    aufg10: aufg10,
-    aufg11_example: aufg11_example,
-    aufg11: aufg11,
-    aufg12: aufg12,
     sin: sin,
     cos: cos,
     tan: tan,
